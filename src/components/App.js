7ce0bf754ff278bb72319763bf900c6ccf2cca15
biwott-v <victor.biwott@student.moringaschool.com>
2025-04-13 13:36:50 +0300

    completes assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import NavBar from "./NavBar.js";
+import Home from "./Home.js";
+import About from "./About.js";
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home/>
+      <About/>
     </div>
   );
 }
